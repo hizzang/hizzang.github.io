03c606a40506dc984bb2708984b7309c1afdfe5f
Seonjo Heo <hizzang+github@Gmail.com>
2026-07-02 08:01:48 +0900

    Add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = (await getCollection('blog'))
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
+  return rss({
+    title: 'hizzang blog',
+    description: '개발, 인프라, 게임 플랫폼 운영 기록을 정리하는 개인 기술 블로그입니다.',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`
+    }))
+  });
+}
